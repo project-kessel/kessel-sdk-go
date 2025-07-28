eac037067e373e2b13b18e88081af762c40a503a
Jonathan Marcantonio <jmarcant@redhat.com>
2025-07-28 11:28:22 -0400

    Reorganize files to follow spec
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- examples/grpc/main.go
+++ examples/grpc/main.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/project-kessel/kessel-sdk-go/kessel/config"
 	"github.com/project-kessel/kessel-sdk-go/kessel/errors"
+	"github.com/project-kessel/kessel-sdk-go/kessel/grpc/auth"
 	v1beta2 "github.com/project-kessel/kessel-sdk-go/kessel/inventory/v1beta2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -17,14 +21,43 @@ func main() {
 	// 2. Issuer-based discovery: Provide the issuer URL and let the SDK discover
 	//    the token endpoint via OpenID Connect Discovery (/.well-known/openid_configuration)
 
-	inventoryClient, err := v1beta2.NewInventoryGRPCClientBuilder().
-		WithEndpoint("127.0.0.1:9000").
-		WithInsecure(true).
-		WithOAuth2Issuer("svc-test", "h91qw8bPiDj9R6VSORsI5TYbceGU5PMH", "http://localhost:8085/realms/redhat-external").
-		WithMaxReceiveMessageSize(8 * 1024 * 1024). // 8MB
-		WithMaxSendMessageSize(8 * 1024 * 1024).    // 8MB
-		Build()
+	grpcConfig := &config.GRPCConfig{
+		BaseConfig: config.BaseConfig{
+			Endpoint:    "127.0.0.1:9000",
+			Insecure:    true,
+			EnableOauth: true,
+			Oauth2: config.Oauth2{
+				ClientID:     "svc-test",
+				ClientSecret: "h91qw8bPiDj9R6VSORsI5TYbceGU5PMH",
+				IssuerURL:    "http://localhost:8085/realms/redhat-external",
+			},
+		},
+		MaxReceiveMessageSize: 8 * 1024 * 1024, // 8MB
+		MaxSendMessageSize:    8 * 1024 * 1024, // 8MB
+	}
 
+	// Create OAuth2 token source
+	tokenSource, err := auth.NewTokenSource(grpcConfig)
+	if err != nil {
+		if errors.IsTokenError(err) {
+			log.Fatal("OAuth2 token configuration failed: ", err)
+		} else {
+			log.Fatal("Unknown auth error: ", err)
+		}
+	}
+
+	// Using insecure credentials for local development
+	var dialOpts []grpc.DialOption
+	dialOpts = append(dialOpts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	dialOpts = append(dialOpts, grpc.WithPerRPCCredentials(tokenSource.GetInsecureGRPCCredentials()))
+
+	dialOpts = append(dialOpts,
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcConfig.MaxReceiveMessageSize)),
+		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(grpcConfig.MaxSendMessageSize)),
+	)
+
+	conn, err := grpc.NewClient(grpcConfig.Endpoint, dialOpts...)
 	if err != nil {
 		// Example of checking for specific error types using sentinel errors
 		if errors.IsConnectionError(err) {
@@ -36,10 +69,12 @@ func main() {
 		}
 	}
 	defer func() {
-		if closeErr := inventoryClient.Close(); closeErr != nil {
+		if closeErr := conn.Close(); closeErr != nil {
 			log.Printf("Failed to close gRPC client: %v", closeErr)
 		}
 	}()
+
+	inventoryClient := v1beta2.NewKesselInventoryServiceClient(conn)
 
 	// Example request using the external API types
 	checkRequest := &v1beta2.CheckRequest{
